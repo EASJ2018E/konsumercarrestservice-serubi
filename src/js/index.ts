8774671c0fc9a41b66e63a2d2ec57761414e58aa
serubi <serubi@live.com>
2018-10-24 10:46:45 +0200

    Tilføjet interface ICar

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,25 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
+// http://rest-pele-easj-dk.azurewebsites.net/api/Cars
+
+interface ICar {
+    model:string;
+    vendor:string;
+    price:number;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+// function greeter(person: Person): string {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+// let user: Person = { firstName: "John", lastName: "Doe" };
+
+// let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+// element.innerHTML = greeter(user);
